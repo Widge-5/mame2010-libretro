1a0cc35a228f7c1c8348accebbe71eb3ec1076f8
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-03-13 04:52:38 -0400

    Update retromain.h

--- src/osd/retro/retromain.h
+++ src/osd/retro/retromain.h
@@ -223,41 +223,6 @@ const kt_table ktable[] = {
    {"-1",       -1,                 ITEM_ID_INVALID},
 };
 
-void process_joypad_state(void)
-{
-   unsigned i, j;
-   for(j = 0;j < 4; j++)
-   {
-      for(i = 0;i < MAX_BUTTONS; i++)
-         joystate[j].button[i] = input_state_cb(j, RETRO_DEVICE_JOYPAD, 0,i)?0x80:0;
-
-            joystate[j].a2[0] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN,  0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_X));
-      joystate[j].a2[1] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN,  0, RETRO_DEVICE_ID_LIGHTGUN_SCREEN_Y));
-
-	joystate[j].a1[0] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN, RETRO_DEVICE_INDEX_ANALOG_LEFT, RETRO_DEVICE_ID_ANALOG_X));
-      joystate[j].a1[1] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN, RETRO_DEVICE_INDEX_ANALOG_LEFT, RETRO_DEVICE_ID_ANALOG_Y));
-
-	   	   //joystate[j].a2[0] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN, RETRO_DEVICE_INDEX_ANALOG_RIGHT, RETRO_DEVICE_ID_ANALOG_X));
-      //joystate[j].a2[1] = 2 * (input_state_cb(j, RETRO_DEVICE_LIGHTGUN, RETRO_DEVICE_INDEX_ANALOG_RIGHT, RETRO_DEVICE_ID_ANALOG_Y));
-	   
-       if (input_state_cb(j, RETRO_DEVICE_LIGHTGUN, 0, RETRO_DEVICE_ID_LIGHTGUN_IS_OFFSCREEN))
-      {
-	//      //top left
-	   joystate[j].a2[0] = -65534;
-	      joystate[j].a2[1] = -65534;
-      }
-
-      //debug
-      //if (joystate[j].a1[0] == 0 && joystate[j].a1[1] == 0)
-      //{
-	 //     joystate[j].a1[0] = 20000;
-	   //   joystate[j].a1[1] = 20000;
-      //}
-
-
-   }
-}
-
 /* Tekken 1/2 */
 #define TEKKEN_LAYOUT	(core_stricmp(machine->gamedrv->name, "tekken") == 0) || (core_stricmp(machine->gamedrv->parent, "tekken") == 0) || \
          		(core_stricmp(machine->gamedrv->name, "tekken2") == 0) || (core_stricmp(machine->gamedrv->parent, "tekken2") == 0)
