0348cb7e0605c6e93d06452387935d561bf54f62
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-03-23 13:10:25 -0400

    CPU overclock part 2

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -352,6 +352,7 @@ void osd_init(running_machine* machine)
 
    prep_retro_rotation(gamRot);
    machine->sample_rate = sample_rate;	/* Override original value */
+   machine->cpu_overclock = cpu_overclock;	/* Override original value */
 
    retro_log(RETRO_LOG_INFO, "[MAME 2010] osd_init done\n");
 }
@@ -544,6 +545,7 @@ void retro_set_environment(retro_environment_t cb)
       { "mame_current_turbo_delay", "Set autofire pulse speed; medium|slow|fast" },
       { "mame_current_frame_skip", "Set frameskip; 0|1|2|3|4|5|6|7|8|9|10|automatic" },
       { "mame_current_sample_rate", "Set sample rate (Restart); 48000Hz|44100Hz|32000Hz|22050Hz" },
+      { "mame_current_cpu_overclock", "Main CPU overclock; default|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|60|65|70|75|80|85|90|95|100|105|110|115|120|125|130|135|140|145|150" },
       { "mame_current_adj_brightness",
 	"Set brightness; default|+1%|+2%|+3%|+4%|+5%|+6%|+7%|+8%|+9%|+10%|+11%|+12%|+13%|+14%|+15%|+16%|+17%|+18%|+19%|+20%|-20%|-19%|-18%|-17%|-16%|-15%|-14%|-13%|-12%|-11%|-10%|-9%|-8%|-7%|-6%|-5%|-4%|-3%|-2%|-1%" },
       { "mame_current_adj_contrast",
